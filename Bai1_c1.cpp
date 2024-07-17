38e64353acde60afc5808a52b0c4d85d4ff7c10c
lengocdinhtri <135338881+lengocdinhtri@users.noreply.github.com>
2024-07-17 17:10:57 +0700

    Add files via upload

---
+++ Bai1_c1.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 15
+
+void TaoMangNgauNhien(int arr[], int size) {
+	srand(time(0));
+	for (int i = 0; i < size; i++) {
+		arr[i] = rand() % 100;  // Tạo số ngẫu nhiên từ 0 đến 99
+	}
+}
+
+int main() {
+	int arr[SIZE];
+	TaoMangNgauNhien(arr, SIZE);
+
+	printf("Mang ngau nhien: ");
+	for (int i = 0; i < SIZE; i++) {
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
---
+++ Bai1_c3.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#define SIZE 15
+
+int LinearSearch(int arr[], int size, int x) {
+	for (int i = 0; i < size; i++) {
+		if (arr[i] == x) {
+			return i;  // Trả về vị trí của x trong mảng
+		}
+	}
+	return -1;  // Trả về -1 nếu không tìm thấy
+}
+
+int main() {
+	int arr[SIZE] = { 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28 };
+	int x = 18;
+	int Ketqua = LinearSearch(arr, SIZE, x);
+
+	if (Ketqua != -1) {
+		printf("Tim thay %d tai vi tri %d\n", x, Ketqua);
+	}
+	else {
+		printf("Khong tim thay so can tim %d\n", x);
+	}
+	return 0;
+}
